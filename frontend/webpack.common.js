d7e6328ecfc49cf508b3a1cad130f9da1c1e9e55
Koen Van den Wijngaert <koen@neok.be>
2020-06-29 07:42:23 +0200

    Working submissions (to localhost)

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -42,7 +42,7 @@ module.exports = {
   },
   plugins: [
     new webpack.ProvidePlugin({
-      fetch: "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
+      fetch: "exports-loader?self.fetch!whatwg-fetch/dist/fetch.umd"
     }),
 
     new AssetsPlugin({
