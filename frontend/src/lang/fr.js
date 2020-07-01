93b38813b6803fa02ec1c67617f14da748f86e2e
Ines <62929810+NiskeFoursevens@users.noreply.github.com>
2020-07-01 21:04:51 +0200

    Update fr.js

--- frontend/src/lang/fr.js
+++ frontend/src/lang/fr.js
@@ -1,2 +1,47 @@
-export default {    
+export default {
+    fields: {
+        contactName: "Votre nom",
+        heroName: "Nom de votre héros du Covid",
+        type: "Voulez-vous partager votre histoire avec nous?",
+        videoUrl: "Lien Youtube",
+        abstract: "Résumé",
+        content: "Votre histoire",
+        contactEmail: "Adresse email",
+        contactPhone: "Numéro de téléphone",
+        gaveConsent: "Autorisation",
+        consentText: "Je donne l’autorisation au Coronadenktank d’utiliser ma vidéo sur les réseaux sociaux, les plateformes de partenaires de PleinDEspoir ainsi que dans la compilation à partager avec les chaines de télévision.",
+    },
+    buttons: {
+        next: "Suivant »",
+        previous: "« Précédent",
+        submit: "Envoyer",
+    },
+    typeLabels: {
+        text: "📝 Texte",
+        video: "🎥 Vidéo",
+    },
+    copy: {
+        lastPageTitle: "On y est presque!",
+        lastPageText: "Comment pouvons-nous vous contacter?",
+    },
+    validation: {
+        required: "Obligatoire",
+        validationPassed: "Merci pour votre histoire. Nous l’avons bien reçue!",
+        validationFailed: "Désolé, plusieurs erreurs se sont produites durant l’envoi. Veuillez les corriger avant de renvoyer vos données.",
+        failedVerification: "Il était impossible de vérifier votre requête. Veuillez essayer ultérieurement.",
+        panicMsg: "Erreur de transmission. Veuillez essayer ultérieurement.",
+        type: "Indiquez votre choix : texte ou vidéo.",
+        contactName: "Remplissez avec un nom valide.",
+        contactEmail: "Remplissez avec une adresse email valide.",
+        heroName: "Remplissez le nom de votre héros.",
+        gaveConsent: "Sans votre autorisation nous ne pouvons pas soumettre votre requête.",
+        abstract: {
+            min: "Votre résumé doit contenir au moins 10 caractères.",
+            max: "Votre résumé ne peut pas dépasser les 300 caractères.",
+        },
+        content: {
+            min: "Votre histoire doit contenir au moins 100 caractères.",
+            max: "Votre histoire ne peut pas dépasser les 2500 caractères.",
+        },
+    }
 };
